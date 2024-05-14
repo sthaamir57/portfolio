dbd255a4707eff6719f4d36dd4da3f503f646982
Amir Shrestha <62130048+sthaamir57@users.noreply.github.com>
2024-05-14 17:05:08 +0545

    removed some lines

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,12 +1,9 @@
-import { Heading } from "../OneForAll/Heading"
+import Heading from "../OneForAll/Heading"
 
 const Works = () => {
   return (
     <div id="myWorks" className="relative h-screen p-16 bg-black/60 backdrop-blur-lg backdrop-saturate-150">
         <Heading heading="Recent Works" />
-        <div className="flex">
-            {/* <div className="w-[2400px] h-[400px] bg-red-600"></div> */}
-        </div>
     </div>
   )
 }
