fd00e58bc10fb86d403394c67349bbbedb576a6f
Amir Shrestha <62130048+sthaamir57@users.noreply.github.com>
2024-05-14 19:51:51 +0545

    added blur to hero section on scroll

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,32 +8,36 @@ const Hero = ({scrollYProgress}) => {
     const x2 = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
     const op = useTransform(scrollYProgress, [0, 1], [1, -2.6]);
     const opSlow = useTransform(scrollYProgress, [0, 1], [1, -1.6]);
-    
+    const blur = useTransform(scrollYProgress, [0, 1], ['blur(0px)', 'blur(20px)']);
+    const blurFast = useTransform(scrollYProgress, [0, 1], ['blur(0px)', 'blur(30px)']);
+
     return (
-        <section className="sticky top-0 flex flex-col items-center justify-center h-screen overflow-hidden">
-            <motion.h2  style={{translateY : x1, opacity: opSlow}} className="flex flex-row items-center gap-2 mb-4">
+        
+        <section className="sticky top-0 flex flex-col items-center justify-center h-screen overflow-hidden ">
+            <motion.h2  style={{translateY : x1, opacity: opSlow,  filter : blur}} className="flex flex-row items-center gap-2 mb-4">
                 <Pulse />
                 <span className="text-2xl tracking-tight cursor-text">Open for work</span>
             </motion.h2>
             <h1 className="leading-[0.92] text-center text-8xl font-display cursor-text">
                 <motion.span
-                    style={{translateX : x1, opacity: op}}
+                    style={{translateX : x1, opacity: op, filter : blurFast}}
                     className="inline-block"
                 >
                     Web Developer
                 </motion.span>
                 <br />
                 <motion.span
-                    style={{translateX : x2, opacity: op}}
+                    style={{translateX : x2, opacity: op, filter : blurFast}}
                     className="inline-block"
                 >& Designer</motion.span>
             </h1>
 
-            <motion.div style={{bottom : x2, opacity: op}} className="absolute flex bottom-0 animate-bounce animate-duration-[25000ms] pb-2">
+            <motion.div style={{bottom : x2, opacity: op, filter : blur}} className="absolute flex bottom-0 animate-bounce animate-duration-[25000ms] pb-2">
                 <span className="inline-block">Recent</span>
                 <ArrowIcon />
                 <span className="inline-block">Works</span>
             </motion.div>
+            
         </section>
     )
 }
