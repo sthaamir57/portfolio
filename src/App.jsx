676422b0da3de75302fe398375671e262f1c1580
Amir Shrestha <62130048+sthaamir57@users.noreply.github.com>
2024-05-17 19:52:15 +0545

    added animation for work section

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,24 @@ function App() {
   }, [])
 
   const container = useRef();
-    const { scrollYProgress } = useScroll({
+  const containerHero = useRef();
+    const { scrollYProgress : Y1 } = useScroll({
       target: container,
       offset: ["start start", "end end"]
     })
+    const { scrollYProgress : Y2 } = useScroll({
+      target: containerHero,
+      offset: ["0 0.2", "0.1 0.1"]
+    })
+
+    
 
   return (
     <>
         <Nav />
         <div className="min-h-[200vh] ">
-          <Hero scrollYProgress={scrollYProgress} ref={container}/>
-          <Works scrollYProgress={scrollYProgress}  />
+          <Hero scrollYProgress={Y1} ref={container}/>
+          <Works scrollYProgress={Y2} ref={containerHero}  />
         </div>
 
         <div id="contact" className="relative h-screen p-16 z-100 bg-slate-800">Contact Me</div>
